51042ef09bcfe938a005bc5ee18f9cb45b712f50
Neo Anoman <neoanoman@gmail.com>
2019-08-20 18:30:13 +0530

    adjusted_carousel

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -3,9 +3,9 @@ console.log("updated");
 function changeME(element) {
   if(element[0]==='.') {
     element=element.substr(1);
-    document.querySelector(".changing_component").innerHTML = '<iframe src="../components/'+element+'.html" width="100%" height="400px" type="text/html" />';
+    document.querySelector(".changing_component").innerHTML = '<iframe src="../components/'+element+'.html" width="100%" height="800px" type="text/html" />';
     }
   else {
-    document.querySelector(".changing_component").innerHTML = '<iframe src="./components/'+element+'.html" width="100%" height="400px" type="text/html" />';
+    document.querySelector(".changing_component").innerHTML = '<iframe src="./components/'+element+'.html" width="100%" height="800px" type="text/html" />';
     }
 }
